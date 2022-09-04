d1ba26893627c1006b2722abd5cb9220522cef5c
URK21CS1070 <100009818+URK21CS1070@users.noreply.github.com>
2022-09-04 21:37:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -109,8 +109,7 @@ for item in input_data.split(', '):
     elif item.startswith('['):
         item = item[1:-1]
         for letter in item.split(','):
-            if letter.isnumeric():
-                inner_list.append(int(letter))
+           
         data.append(inner_list)
 
 obj = DoublyCircularLinkedList()
